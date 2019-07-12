23c5dfb68bb686e8a4bafbaa04bc49b416536344
rushi444 <rushi44@gmail.com>
2019-07-12 09:39:16 -0700

    More fixes to plural bug

--- src/piggyBankApp/Money.java
+++ src/piggyBankApp/Money.java
@@ -4,11 +4,11 @@ public abstract class Money {
     private int quantity;
     private double value;
     private String type;
-    private String plural;
+    
 
     public Money() {
         this.quantity = 1;
-        this.plural = "";
+
     }
 
     public Money(int quantity) {
